42fe88ee293b57e2e49f4fe72a2d08fe27e87ad6
rocky254 <49587651+rocky254@users.noreply.github.com>
2019-09-14 09:51:41 +0300

    Add files via upload

---
+++ Revision/Demo.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int i;
+	void f1(void);
+	/*cin >> i >> end1;
+	i=0;
+	cout<<"Value of i in main: "<<i<<end1;
+	f1();
+	cout<<"Value of i after call: "<<i<<end1;
+}
+
+void f1(){
+	int i=0;
+	i=50;
+}
+*/
